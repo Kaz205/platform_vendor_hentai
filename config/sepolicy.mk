e4bcb68d32b2fac74fa92f19705552df44a9ba42
Dyneteve <dyneteve@hentaios.com>
2025-01-07 19:48:46 +0000

    config: Move flipendo sepolicy to system_ext
    Change-Id: I93e760998977f7b9f5368577eb49a4ce2df65d57
    Signed-off-by: Dyneteve <dyneteve@hentaios.com>

--- config/sepolicy.mk
+++ config/sepolicy.mk
@@ -23,7 +23,5 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
 	hardware/google/pixel-sepolicy/turbo_adapter/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+	hardware/google/pixel-sepolicy/flipendo \
 	hardware/google/pixel-sepolicy/turbo_adapter/private
-
-BOARD_SEPOLICY_DIRS += \
-	hardware/google/pixel-sepolicy/flipendo
